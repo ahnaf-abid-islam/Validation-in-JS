95c85f3540ecca8e4dfb0a7b9dc789b2a0af6069
ahnaf-abid-islam <ahnafabid.islam@gmail.com>
2025-08-22 22:47:21 +0600

    calculator JS

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 function myFunction(){
     var inpValid = document.getElementById("inp1");
     if (!inpValid.checkValidity()){
-        document.getElementById("result").innerHTML = inpValid.validationMessage;
+        document.getElementById("result1").innerHTML = inpValid.validationMessage;
     }
     else{
-        document.getElementById("result").innerHTML = "Atleast you have a brain to complete this";
+        document.getElementById("result1").innerHTML = "Atleast you have a brain to complete this";
+    }
+}
+
+const display = document.getElementById("result");
+
+function setScreenValue(value){
+    display.value += value;
+}
+
+function clearScreen(){
+    display.value = "";
+}
+
+function calculateResult(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error Detected";
     }
 }
